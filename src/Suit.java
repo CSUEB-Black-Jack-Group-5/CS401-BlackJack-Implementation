57105a58f906212e3df09f0dd76542ffd6e832a5
Martin Garcia <martingarcia9931@gmail.com>
2025-04-13 10:16:11 -0700

    Feature card (#3)
    * implement card class skeleton, suit and value enums based on uml class diagram.
    
    * implement card class skeleton, suit and value enums based on uml class diagram.

---
+++ src/Suit.java
@@ -0,0 +1,15 @@
+public enum Suit {
+    CLUBS,
+    HEARTS,
+    SPADES,
+    DIAMONDS;
+//edit
+    public int getValue(){
+        return switch (this) {
+            case CLUBS -> 0;
+            case HEARTS -> 1;
+            case SPADES -> 2;
+            case DIAMONDS -> 3;
+        };
+    }
+}
