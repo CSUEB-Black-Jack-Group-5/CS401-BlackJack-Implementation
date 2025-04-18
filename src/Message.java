6b5852dded8ebfefa75998d5351d16c674162c29
coffeacat <michellenguyen602@gmail.com>
2025-04-18 12:27:27 -0700

    added Login Message Request and Response

--- src/Message.java
+++ src/Message.java
@@ -2,8 +2,40 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     public static class Login {
-        public static class Request extends Message {}
-        public static class Response extends Message {}
+        public static class Request extends Message {
+            private String username;
+            private String password;
+
+            public Request (String username, String password) {
+                this.username = username;
+                this.password = password;
+            }
+
+            public String getUsername() {
+                return username;
+            }
+
+            public String getPassword() {
+                return password;
+            }
+        }
+        public static class Response extends Message {
+            private boolean status;
+            private AccountType type;       // this doesn't exist yet
+
+            public Response (boolean status, AccountType type) {
+                this.status = status;
+                this.type = type;
+            }
+
+            public boolean getStatus() {
+                return status;
+            }
+
+            public AccountType getType() {
+                return type;
+            }
+        }
     }
     public static class Hit {
         public static class Request extends Message {}
