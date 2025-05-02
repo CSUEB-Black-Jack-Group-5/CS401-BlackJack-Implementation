40e99caaa8eab3135df0b2ebdb1009fc0261ed34
coffeacat <michellenguyen602@gmail.com>
2025-05-01 19:49:44 -0700

    Fixing my initial push / Updating the Lobby and Table class

--- src/game/Lobby.java
+++ src/game/Lobby.java
@@ -1,4 +1,85 @@
 package game;
 
 public class Lobby {
+    // not sure whether to transfer this into an array list or keep it as an array
+    private Table[] tables;
+    private int tableCount;
+    private static final int TABLE_LIMIT = 100;
+    // private ConnectionHandler connections according to the UML
+    // figure out how to integrate with server connections / message response later?
+
+    /* CONSTRUCTOR */
+    public Lobby(){
+        this.tables = new Table[TABLE_LIMIT];
+    }
+
+    /* RETURNS THE LIST OF TABLES */
+    public Table[] getTables() {
+        return tables;
+    }
+
+    /* RETURNS THE NUMBER OF TABLES IN THE LOBBY */
+    public int getTableCount() {
+        return tableCount;
+    }
+
+    /* RETURNS THE LIMIT OF TABLES IN A LOBBY */
+    // remove this if converting lobby into an array list
+    public int getTableLimit() {
+        return TABLE_LIMIT;
+    }
+
+    /* CREATES A NEW TABLE AT "THE END" OF THE ARRAY */
+    public void addTable(Dealer dealer) {
+        tables[tableCount] = new Table(dealer);
+        tableCount++;
+    }
+
+    // identify by dealer username
+    // or maybe i should do table id?
+    public void removeTable(Dealer dealer) {
+
+        // is the table in the collection?
+        int match = -1;		// will hold the index for the table if found
+        // goes through the entire array looking for a matching table
+        for (int i = 0; i < tableCount; i++) {
+            // if there is a matching dealer at the table
+            if (tables[i].getDealer().getUsername().equals(dealer.getUsername())) {
+                match = i;	// stores the index
+                break;		// and exits the loop
+            }
+        }
+
+        // if the table exists within the lobby
+        if (match > -1) {
+            // calls the close session method in the table class (for logging purposes?)
+            tables[match].closeSession();
+            // and makes the table inaccessible and available for garbage collection
+            // overwrites the "deleted" element with the next one over
+            for (int i = match; i < tableCount-1; i++) {
+                tables[i] = tables[i + 1];
+            }
+            tableCount--;			// changes the table count to be accurate
+            tables[tableCount] = null;	/* not that it matters much
+             * since it's inaccessible from the public interface, but
+             * deallocates the original last element of the past array */
+        }
+
+        // v THIS IS JUST HERE FOR TESTING PURPOSES, I'LL REMOVE THIS WHEN THE FINAL DESIGN IS COMPLETE!!
+        // if there was no table removed
+        if (match == -1) {
+            System.out.print("Table Remove Operation Failed.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String text = "LOBBY:\n\n";
+
+        for (int i = 0; i < tableCount; i++) {
+            text += tables[i].toString();
+        }
+
+        return text;
+    }
 }
--- src/game/Table.java
+++ src/game/Table.java
@@ -2,68 +2,69 @@ package game;
 
 import java.time.LocalDateTime;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Table1 {
+public class Table {
     protected static int idCount = 0;
     private int tableId;
     private Dealer dealer;
     private Player[] players;
-    private boolean[] ready; // do i need this?
-    private static final int PLAYER_LIMIT = 6;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private int duration;
-    private static Timer turnTimer;
     private int playerCount;
-    private boolean isActive;
+    private static final int PLAYER_LIMIT = 6;
+    private static Timer turnTimer;
+    private LocalDateTime startTime;    // extrapolation from GameData class; not sure if needed
+    private LocalDateTime endTime;      // extrapolation from GameData class; not sure if needed
+    private int timeLive;               // extrapolation from GameData class; not sure if needed
+    private boolean isActive;           // this is a variable used to determine whether
+                                        // the table is within a game or not
+    // thought it would be helpful for handling incoming connections during a game
 
     // how do we make sure that dealer only spawns one table?
     // maybe flag in the dealer class that triggers if they have a table open already?
     // actually i don't even know if we want this feature ^^ we'll deal with it later if we do want this
-    public Table1(Dealer dealer){
+    /* CONSTRUCTOR */
+    public Table(Dealer dealer){
         this.tableId = idCount++;
         this.dealer = dealer;
         this.players = new Player[PLAYER_LIMIT];
-        this.ready = new boolean[PLAYER_LIMIT];
-        this.startTime = LocalDateTime.now();
         this.playerCount = 0;
-        this.isActive = true;
+        this.turnTimer = new Timer(60);
+        this.startTime = LocalDateTime.now();
+        this.isActive = false;
     }
 
+    /* GETTERS */
     public int getTableId() {
         return tableId;
     }
 
     public Dealer getDealer() {
-        return this.dealer;
+        return dealer;
     }
 
     public Player[] getPlayers() {
-        return this.players;
-    }
-
-    public boolean[] getReady() {
-        return ready;
-    }
-
-    // idk if we need this, but it's in the UML so
-    // also as far as i know, getUsername is not a getter in the Dealer class
-    public String getDealerName() {
-        return this.dealer.getUsername();
+        return players;
     }
 
     public int getPlayerCount() {
-        return this.playerCount;
+        return playerCount;
     }
 
     public int getPlayerLimit() {
         return PLAYER_LIMIT;
     }
 
+    public static Timer getTurnTimer() {
+        return turnTimer;
+    }
+
+    // checks if the table is in a game
     public boolean isActive() {
         return this.isActive;
     }
 
+    /* CHECKS IF THE TABLE IS FULL */
     public boolean isFull() {
         if (playerCount >= PLAYER_LIMIT) {
             return true;
@@ -72,40 +73,173 @@ public class Table1 {
         }
     }
 
-// im actually not sure how to handle player moves but :sob: this is my attempt at hit handling
+    /* CHECKS IF THE ENTIRE TABLE IS READY */
+    public boolean tableReady() {
+        boolean tableReady = true;
+        // loops through the player list
+        for (int i = 0; i < playerCount; i++) {
+            // if a player isn't ready, the table is not ready
+            if (!players[i].isReady()) {
+               tableReady = false;
+            }
+        }
+        return tableReady;
+    }
 
-    resetTable()
+    /* ADDS A PLAYER TO THE ARRAY */
+    public void addPlayer(Player player) {
+        // if the table is not full
+        if (!isFull()) {
+            // adds a player
+            players[playerCount] = player;
+            playerCount++;
+        }
+    }
 
+    /* REMOVES A PLAYER FROM THE ARRAY AND SHIFTS ANY PLAYERS AFTER FORWARD */
+    public void removePlayer(Player player) {
+        // is the player in the array?
+        int match = -1;		// will hold the index for the player if found
+        // goes through the entire array looking for the player
+        for (int i = 0; i < playerCount; i++) {
+            // if there is a matching player
+            if (players[i].getUsername().equals(player.getUsername())) {
+                match = i;	// stores the index
+                break;		// and exits the loop
+            }
+        }
 
-    public void closeTable() {
+        /* if the player exists within the array
+         * makes the player object inaccessible and available for garbage collection */
+        if (match > -1) {
+            // overwrites the "deleted" element with the next one over
+            for (int i = match; i < playerCount-1; i++) {
+                players[i] = players[i + 1];
+            }
+            playerCount--;			// changes the player count to be accurate
+            players[playerCount] = null;	/* not that it matters much
+             * since it's inaccessible from the public interface, but
+             * deallocates the original last element of the past array */
+        }
+
+        // v THIS IS JUST HERE FOR TESTING PURPOSES, I'LL REMOVE THIS WHEN THE FINAL DESIGN IS COMPLETE!!
+        // if there was no player removed
+        if (match == -1) {
+            System.out.print("Player Remove Operation Failed.");
+        }
+    }
+
+    /* MAIN GAMEPLAY LOOP */
+    public void startGame() {
+        // should I assume that readyCheck is triggered beforehand and remove this?
+        if (!tableReady()) {
+            return;
+        }
+
+        // game is active
+        isActive = true;
+
+        // creates a list of players to deal to using the player array
+        List<Player> dealToPlayers = new ArrayList<>();
+        for (Player element : players) {
+            if (element != null) {
+                dealToPlayers.add(element);
+            }
+        }
+
+        // deals two cards to each player
+        dealer.deal(dealToPlayers);
+        // deal() method takes in a list of players,
+        // hence the array to list conversion
+
+        // for each player's turn
+        for (int i = 0; i < playerCount; i++) {
+            // start the timer
+            turnTimer.startTimer();
+
+            // keep track of the timer to make sure it doesn't go over the duration
+            while(!turnTimer.isTimeUp() /* OR --> insert condition for player has made a move */ ) {
+                /* I HAVE NO IDEA IF PLAYER MOVES ARE HANDLED HERE OR NOT!!! */
+                /* not sure if blackjack check is run here or not either */
+            }
+
+            // if time is up or player has made a move
+            // stops the timer
+            turnTimer.stopTimer();
+            // and resets it for the next player
+            turnTimer.resetTimer();
+        }
+
+        /* INSERT DEALER SEQUENCE AT THE END + CALL PAYOUT */
+
+        // game is no longer active
+        isActive = false;
+
+        resetTable();
+    }
+
+    /* RESETS THE TABLE STATE */
+    public void resetTable() {
+
+        // resets the timer just in case
+        turnTimer.resetTimer();
+
+        // all players ready state is set to false and reset their hands
+        for (int i = 0; i < playerCount; i++) {
+            players[i].setReady(false);
+            players[i].resetHand();
+        }
+
+        // resets the dealer's hand
+        dealer.resetHand();
+
+        // not sure if i want to call shuffle at the end or not
+
+    }
+
+    /* IF DEALER LEAVES, CALL THIS METHOD TO LOG TABLE DATA */
+    public void closeSession() {
         this.endTime = LocalDateTime.now();
         Duration elapsedTime = Duration.between(startTime, endTime);
-        this.duration = (int) elapsedTime.toMinutes();
+        this.timeLive = (int) elapsedTime.toMinutes();
+
+        /* INSERT DATABASE METHOD HERE */
+        // clearTableData();?
+
+        // goes back to the lobby closeTable method
+    }
+
+    /* THIS IS FOR TESTING PURPOSES; RESETS TO THE CONSTRUCTOR STATE */
+    public void clearTableData() {
+        for (int i = 0; i < playerCount; i++) {
+            removePlayer(players[i]);
+        }
+        playerCount = 0;
+        turnTimer.resetTimer();
+        isActive = false;
     }
 
     @Override
-    public String toString() {
-        return "Dealer: " + this.dealer.getUsername() + "\nTable Id: " + this.tableId + "\n";
-    }
-
-    /*public static void join(Player player) {
-        this.players[this.playerCount] = player;
-        this.ready[this.playerCount] = false;
-        this.playerCount++;
-    }
-
-    public static void disconnect(Player player) {
-
-    }
-
-    public static void startGame() {
-        Game newGame = new Game();
-        this.games[this.gameCount] = newGame;
-    }
-
-    // include a method that allows for easy logging
+    // include a method that allows for easy logging / testing
     // returns the current state of the table
-    public static String toString() {
-        return // INSERT FORMAT HERE
-    }*/
+    // INSERT FORMAT HERE
+    public String toString() {
+        String data = "Table Id: " + this.tableId + "\nDealer: " +
+                this.dealer.getUsername() + "\nPlayers:\n";
+        if (playerCount == 0) {
+            data += "NONE\n";
+        } else {
+            for (int i = 0; i < playerCount; i++) {
+                data += "[" + (i + 1) + "] " + players[i].getUsername() + "\n\tReady Status: ";
+                if (players[i].isReady()) {
+                    data += "READY\n";
+                } else {
+                    data += "NOT READY\n";
+                }
+            }
+        }
+        data += "\n";
+
+        return data;
+    }
 }
