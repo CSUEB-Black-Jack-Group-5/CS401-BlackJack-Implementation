9c3ed3c67709758661b62286afed00f9b807a954
CodeLabTemp <martingarcia9931@gmail.com>
2025-05-05 15:41:07 -0700

    just throwing code at the screen and see what works

--- src/server/TableThread.java
+++ src/server/TableThread.java
@@ -1,11 +1,20 @@
 package server;
 
 import game.Dealer;
+import game.Player;
 import game.Table;
 import networking.Message;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TableThread implements Runnable {
     private static final int MAX_JOINED_USERS = 6;
+
+    private final Map<String, Integer> playerBets = new ConcurrentHashMap<>();
+
 
     // Table table;
     ClientThreadWithHooks[] joinedUsers;
@@ -19,16 +28,20 @@ public class TableThread implements Runnable {
     }
 
     @Override
-    public void run() {}
+    public void run() {
+
+    }
 
     public Table getTable() {
         return table;
     }
 
+
     public void addClientToTable(ClientThreadWithHooks clientThreadWithHooks) {
         if (joinedUsersCount < MAX_JOINED_USERS) {
             joinedUsers[joinedUsersCount++] = clientThreadWithHooks;
             clientThreadWithHooks.setActiveTable(this);
+
             // let client know they joined the table
             clientThreadWithHooks.sendNetworkMessage(new Message.JoinTable.Response(true));
         } else {
@@ -36,4 +49,14 @@ public class TableThread implements Runnable {
             clientThreadWithHooks.sendNetworkMessage(new Message.JoinTable.Response(false));
         }
     }
+    public void placeBet(String username, int bet) {
+        playerBets.put(username, bet);
+    }
+    public boolean allPlayersBet() {
+        return playerBets.size() == joinedUsersCount;
+    }
+    public void resetBets() {
+        playerBets.clear();
+    }
+
 }
