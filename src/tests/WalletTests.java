b12f552f50bdcb477e2c16934a2635044277d2a2
Riley Fischer <rfmineguy@gmail.com>
2025-05-06 08:38:52 -0700

    Downgraded WalletTests from Junit 5 to Junit 4

--- src/tests/WalletTests.java
+++ src/tests/WalletTests.java
@@ -1,31 +1,32 @@
 import game.Wallet;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class WalletTests {
     @Test
     public void wallet_constructor_and_getFunds() {
         Wallet wallet = new Wallet();
-        Assertions.assertEquals(0.0f, wallet.getFunds());
+        assertEquals(0.0f, wallet.getFunds(), 0.001);
     }
 
     @Test
     public void wallet_addFunds() {
         Wallet wallet = new Wallet();
         wallet.addFunds(1.0f);
-        Assertions.assertEquals(1.0f, wallet.getFunds());
+        assertEquals(1.0f, wallet.getFunds(), 0.001);
 
         wallet.addFunds(45.42f);
-        Assertions.assertEquals(46.42f, wallet.getFunds());
+        assertEquals(46.42f, wallet.getFunds(), 0.01);
     }
 
     @Test
     public void wallet_removeFunds() {
         Wallet wallet = new Wallet();
         wallet.addFunds(45.0f);
-        Assertions.assertTrue(wallet.removeFunds(32.0f));
-        Assertions.assertEquals(13.0f, wallet.getFunds());
-        Assertions.assertFalse(wallet.removeFunds(45.0f));
-        Assertions.assertEquals(13.0f, wallet.getFunds());
+        assertTrue(wallet.removeFunds(32.0f));
+        assertEquals(13.0f, wallet.getFunds(), 0.001);
+        assertFalse(wallet.removeFunds(45.0f));
+        assertEquals(13.0f, wallet.getFunds(), 0.001);
     }
 }
