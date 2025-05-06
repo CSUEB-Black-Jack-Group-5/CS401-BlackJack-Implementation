22eef22be5ff27e0cb9888ac809d1ff9b8dca9f2
Riley Fischer <rfmineguy@gmail.com>
2025-05-06 09:22:26 -0700

    Made addFunds/removeFunds public :facepalm:

--- src/game/Wallet.java
+++ src/game/Wallet.java
@@ -13,7 +13,7 @@ public class Wallet implements Serializable {
      * @param delta How much money to add to the wallet
      * @apiNote delta is expected to be greater than or equal to 0
      */
-    void addFunds(float delta) {
+    public void addFunds(float delta) {
         assert delta >= 0;          // NOTE: I'm not sure if this is absolutely necessary - Riley Fischer
         this.funds += delta;
     }
@@ -22,7 +22,7 @@ public class Wallet implements Serializable {
      * @param delta How much money to remove from the wallet
      * @apiNote delta is expected to be less than or equal to 0
      */
-    void removeFunds(float delta) {
+    public void removeFunds(float delta) {
         assert delta <= 0;          // NOTE: I'm not sure if this is absolutely necessary - Riley Fischer
         this.funds += delta;
     }
